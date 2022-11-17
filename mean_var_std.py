2b3f7a878e22bddea7ba1d65e3d5676be28257cc
Aadit Kamat <>
2022-11-17 22:12:18 +0800

    Add solution for project

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
+    # If length of the list is not 9, return an error
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    # Convert list into a 3x3 numpy array
+    ar = np.array(list).reshape((3, 3))
+    # Initialize calculations variable with dictionary in given format
+    calculations = {
+      'mean': [ar.mean(axis=0).tolist(), ar.mean(axis=1).tolist(), ar.mean().tolist()],
+      'variance': [ar.var(axis=0).tolist(), ar.var(axis=1).tolist(), ar.var().tolist()],
+      'standard deviation': [ar.std(axis=0).tolist(), ar.std(axis=1).tolist(), ar.std().tolist()],
+      'max': [ar.max(axis=0).tolist(), ar.max(axis=1).tolist(), ar.max().tolist()],
+      'min': [ar.min(axis=0).tolist(), ar.min(axis=1).tolist(), ar.min().tolist()],
+      'sum': [ar.sum(axis=0).tolist(), ar.sum(axis=1).tolist(), ar.sum().tolist()],
+    }
     return calculations
+
+print(calculate([0,1,2,3,4,5,6,7,8]))
